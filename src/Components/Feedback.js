c5db9e004aa4aee1b5c9f3bf95762848d8646dc7
Wilfredo <casas.farach@gmail.com>
2020-10-30 00:52:05 +0100

    feedback form

--- src/Components/Feedback.js
+++ src/Components/Feedback.js
@@ -4,32 +4,35 @@ import * as firebase from "firebase";
 export default function Footer() {
   const store = firebase.firestore();
   const [name, setName] = useState(null);
+  const [email, setEmail] = useState(null);
+  const [feedback, setFeedback] = useState(null);
+
 
   const handleChange = (e) => {
     setName(e.target.value);
   };
 
-  const handleSubmit = async (e) => {
-    alert("hello");
-    // if (name === "" || feedback === "") return setError("empty");
-    await store.collection("feedback").add({
-      name: name,
-      time: Date.now(),
-    });
+  const handleSubmit = (e) => {
     e.preventDefault();
-
+    // if (name === "" || feedback === "") return setError("empty");
+    store.collection("feedback").add({
+      name,
+      email,
+      feedback
+    });
   };
 
   return (
     <>
       <h3>Leave your feedback here</h3>
-      <p>
-        Idea for a new game? Something you didn't like? Found an error? This is
-        the place to say it.
-      </p>
+      <p>Idea for a new game? Something you didn't like? Found an error?</p>
+      <p> This is the place to say it.</p>
       <p>Your feedback will be read directly by me (Wilfredo).</p>
-      <form onSubmit={handleSubmit}>
-        <input type="text" name="name" value={name} onChange={handleChange} />
+      <form  onSubmit={handleSubmit}>
+        <input placeholder="Name" type="text" name="name" value={name} onChange={handleChange} />
+        <input placeholder="Email" type="email" name="email" value={email} onChange={handleChange} />
+        <textarea placeholder="Speak your mind here :)" type="text"           rows={5}
+ name="feedback" value={feedback} onChange={handleChange} />
         <input type="submit" value="Submit" />
       </form>
     </>
