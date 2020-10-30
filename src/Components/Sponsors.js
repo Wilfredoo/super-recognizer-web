cb4c97c74e1b05a1942b0020880d2459731121bc
Wilfredo <casas.farach@gmail.com>
2020-10-30 18:50:22 +0100

    sponsor page

--- src/Components/Sponsors.js
+++ src/Components/Sponsors.js
@@ -4,7 +4,7 @@ import "../App.scss";
 export default function Sponsors() {
   return (
     <div className="componentContainer">
-      <h2>Especially excited about this project?</h2>
+      <h2>Excited about this project?</h2>
       <p>
         This project is funded with personal money (which is not that much) and
         those of my sponsors.
@@ -24,32 +24,40 @@ export default function Sponsors() {
         That way I can work on it more time and make it much better than it is
         right now.
       </p>
-      <h2>Current Sponsors</h2>
 
-      <div>
+      <div className="current-sponsors-div">
+        <div className="current-sponsors-line">
+          <img className="torch-icon" src="torch.png" />
+
+          <h2 className="current-sponsors">Current Sponsors</h2>
+          <img className="torch-icon" src="torch.png" />
+        </div>
+      </div>
+
+      <div class="sponsors-container">
         <div className="sponsor-line">
           <img src="enthusiast.png" className="enthusiast" />{" "}
           <h3 className="sponsor-type enthusiastText">Enthusiasts</h3>
-          <img src="enthusiast.png" className="enthusiast" />
+          <img src="enthusiastB.png" className="enthusiast" />
         </div>
         <p>Maria Farach (mom)</p>
         <p>Wilfredo Casas (dad)</p>
       </div>
-      <div>
+      <div class="sponsors-container">
         <div className="sponsor-line">
-          <img src="supporter.png" className="supporter" />
+          <img src="supporterB.png" className="supporter" />
           <h3 className="sponsor-type supporterText">Supporters</h3>
           <img src="supporter.png" className="supporter" />
         </div>
-        <p>No supporters yet. Be the first? :)</p>
+        <p>...no supporters yet. Be the first?</p>
       </div>
-      <div>
+      <div class="sponsors-container">
         <div className="sponsor-line">
-          <img src="champion.png" className="champion" />
+          <img src="championB.png" className="champion" />
           <h3 className="sponsor-type championText">Champions</h3>
           <img src="champion.png" className="champion" />
         </div>
-        <p>No champions yet. Be the first? :D</p>
+        <p>...no champions yet. Be a champion?</p>
       </div>
     </div>
   );
