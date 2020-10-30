69300bbe6c9d04502b1fd27bf6ce3f8a2bd28518
Wilfredo <casas.farach@gmail.com>
2020-10-30 18:20:59 +0100

    sponsor page progress

--- src/Components/Sponsors.js
+++ src/Components/Sponsors.js
@@ -14,12 +14,11 @@ export default function Sponsors() {
         a month via Patreon.
       </p>
       <div className="sponsor-div">
-        <img className="laurel-icon" src="laurel.png"/>
-      <a href="https://www.patreon.com/super_recogniser" target="_blank">
-        <button class="sponsor-button">Sponsor</button>
-      </a>
-      <img className="laurel-icon" src="laurel.png"/>
-
+        <img className="laurel-icon" src="laurel.png" />
+        <a href="https://www.patreon.com/super_recogniser" target="_blank">
+          <button class="sponsor-button">Sponsor</button>
+        </a>
+        <img className="laurel-icon" src="laurel.png" />
       </div>
       <p>
         That way I can work on it more time and make it much better than it is
@@ -28,16 +27,28 @@ export default function Sponsors() {
       <h2>Current Sponsors</h2>
 
       <div>
-        <h3 className="sponsor-type enthusiasts">Enthusiasts</h3>
+        <div className="sponsor-line">
+          <img src="enthusiast.png" className="enthusiast" />{" "}
+          <h3 className="sponsor-type enthusiastText">Enthusiasts</h3>
+          <img src="enthusiast.png" className="enthusiast" />
+        </div>
         <p>Maria Farach (mom)</p>
         <p>Wilfredo Casas (dad)</p>
       </div>
       <div>
-        <h3 className="sponsor-type supporters">Supporters</h3>
+        <div className="sponsor-line">
+          <img src="supporter.png" className="supporter" />
+          <h3 className="sponsor-type supporterText">Supporters</h3>
+          <img src="supporter.png" className="supporter" />
+        </div>
         <p>No supporters yet. Be the first? :)</p>
       </div>
       <div>
-        <h3 className="sponsor-type champions">Champions</h3>
+        <div className="sponsor-line">
+          <img src="champion.png" className="champion" />
+          <h3 className="sponsor-type championText">Champions</h3>
+          <img src="champion.png" className="champion" />
+        </div>
         <p>No champions yet. Be the first? :D</p>
       </div>
     </div>
