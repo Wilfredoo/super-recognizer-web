0e82c9456c41b1eed84e83684772c111c69fe18b
Wilfredo <casas.farach@gmail.com>
2020-11-02 22:29:40 +0100

    squeezed text

--- src/Components/Sponsors.js
+++ src/Components/Sponsors.js
@@ -4,6 +4,51 @@ import "../App.scss";
 export default function Sponsors() {
   return (
     <div className="componentContainer">
+      <div className="current-sponsors-div">
+        <div className="current-sponsors-line">
+          <img className="torch-icon" src="torch.png" />
+
+          <h2 className="current-sponsors">Current Sponsors</h2>
+          <img className="torch-icon" src="torch.png" />
+        </div>
+      </div>
+
+      <div class="sponsors-container">
+        <div className="sponsor-line">
+          <img src="enthusiast.png" className="enthusiast" />{" "}
+          <h3 className="sponsor-type enthusiastText">Enthusiasts</h3>
+          <img src="enthusiastB.png" className="enthusiast" />
+        </div>
+        <p>Maria Farach</p>
+        <p>Antonio Casas</p>
+      </div>
+      <div class="sponsors-container">
+        <div className="sponsor-line">
+          <img src="supporterB.png" className="supporter" />
+          <h3 className="sponsor-type supporterText">Supporters</h3>
+          <img src="supporter.png" className="supporter" />
+        </div>
+        <p>
+          ...no supporters yet.{" "}
+          <a href="https://www.patreon.com/super_recogniser" target="_blank">
+            <span>Be the first?</span>
+          </a>
+        </p>
+      </div>
+      <div class="sponsors-container">
+        <div className="sponsor-line">
+          <img src="championB.png" className="champion" />
+          <h3 className="sponsor-type championText">Champions</h3>
+          <img src="champion.png" className="champion" />
+        </div>
+        <p>
+          ...no champions yet.{" "}
+          <a href="https://www.patreon.com/super_recogniser" target="_blank">
+            <span> Be a champion?</span>
+          </a>
+        </p>
+      </div>
+      <p>...</p>
       <h2 className="feedback-title">I love doing this!</h2>
       <p>
         But it's hard work and it takes time to upload all these faces ya'll
@@ -31,43 +76,6 @@ export default function Sponsors() {
 
       <p>Much appreciated!</p>
       <p>- Wilfredo</p>
-
-      <p>...</p>
-
-      <div className="current-sponsors-div">
-        <div className="current-sponsors-line">
-          <img className="torch-icon" src="torch.png" />
-
-          <h2 className="current-sponsors">Current Sponsors</h2>
-          <img className="torch-icon" src="torch.png" />
-        </div>
-      </div>
-
-      <div class="sponsors-container">
-        <div className="sponsor-line">
-          <img src="enthusiast.png" className="enthusiast" />{" "}
-          <h3 className="sponsor-type enthusiastText">Enthusiasts</h3>
-          <img src="enthusiastB.png" className="enthusiast" />
-        </div>
-        <p>Maria Farach</p>
-        <p>Antonio Casas</p>
-      </div>
-      <div class="sponsors-container">
-        <div className="sponsor-line">
-          <img src="supporterB.png" className="supporter" />
-          <h3 className="sponsor-type supporterText">Supporters</h3>
-          <img src="supporter.png" className="supporter" />
-        </div>
-        <p>...no supporters yet. Be the first?</p>
-      </div>
-      <div class="sponsors-container">
-        <div className="sponsor-line">
-          <img src="championB.png" className="champion" />
-          <h3 className="sponsor-type championText">Champions</h3>
-          <img src="champion.png" className="champion" />
-        </div>
-        <p>...no champions yet. Be a champion?</p>
-      </div>
     </div>
   );
 }
