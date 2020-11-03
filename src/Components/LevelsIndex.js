2cc99b39df489fc8bdd9274ad9c3a956d023c429
Wilfredo <casas.farach@gmail.com>
2020-11-03 19:12:31 +0100

    descriptions, disclaimer

--- src/Components/LevelsIndex.js
+++ src/Components/LevelsIndex.js
@@ -1,48 +1,79 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import "../App.scss";
+import React from 'react';
+import { Link } from 'react-router-dom';
+import '../App.scss';
 
 function LevelsIndex({
-  match: {
-    params: { game },
-  },
+	match: {
+		params: { game },
+	},
 }) {
-  const levelsArray = ['I', 'II', 'III'];
-  const celebrities = [
-    "Trump",
-    "Shakira",
-    "Bruno Mars",
-    "Ed Sheeran",
-    "Taylor Swift",
-    "Tom Cruise",
-    "Keanu Reeves",
-    "Kit Harrington",
-    "Lili Reinhart",
-    "Michelle Obama",
-  ];
-  return (
-    <div className="componentContainer">
-      <h3>Levels</h3>
-      {game !== "celebrity-match" ?
-        levelsArray.map((level) => {
-          return (
-            <div>
-              <Link to={`/game/${game}/${level}`}>
-                <p className="levels">Level {level}</p>
-              </Link>
-            </div>
-          );
-        }) : celebrities.map((celebrity) => {
-          return (
-            <div>
-              <Link to={`/game/${game}/${celebrity}`}>
-                <p className="celebrity-levels">{celebrity}</p>
-              </Link>
-            </div>
-          );
-        })}
-    </div>
-  );
+	const levelsArray = ['I', 'II', 'III'];
+	const celebrities = [
+		'Trump',
+		'Shakira',
+		'Bruno Mars',
+		'Ed Sheeran',
+		'Taylor Swift',
+		'Tom Cruise',
+		'Keanu Reeves',
+		'Kit Harrington',
+		'Lili Reinhart',
+		'Michelle Obama',
+	];
+	return (
+		<div className="componentContainer">
+			{game === 'celebrity-match' && (
+				<>
+					<h3>Celebrity Match</h3>
+					<p>Can you differentiate between the real celebrity and the imposter?</p>
+				</>
+			)}
+			{game === 'remember-the-face' && (
+				<>
+					<h3>Remember the Face</h3>
+					<p>You'll be shown faces of strangers. Can you remember them correctly?</p>
+				</>
+			)}
+
+			{game === 'world-of-averages' && (
+				<>
+					<h3>World of Averages</h3>
+					<p>How good are you at differentiating facial features from different ethnicities?</p>
+				</>
+			)}
+			<h3>Levels</h3>
+			{game !== 'celebrity-match'
+				? levelsArray.map((level) => {
+						return (
+							<div>
+								<Link to={`/game/${game}/${level}`}>
+									<p className="levels">Level {level}</p>
+								</Link>
+							</div>
+						);
+				  })
+				: celebrities.map((celebrity) => {
+						return (
+							<div>
+								<Link to={`/game/${game}/${celebrity}`}>
+									<p className="celebrity-levels">{celebrity}</p>
+								</Link>
+							</div>
+						);
+				  })}
+			<p>...</p>
+			{game === 'world-of-averages' && (
+				<p className="disclaimer">
+					{' '}
+					DISCLAIMER: this game does not intend to offend anyone or promote any kind of discrimination (I am
+					part of a minority group myself). The photos are just averages from a relatively small sample of
+					people of each country and the sole purpose of this game is training. I take personal responsability
+					for anyone that I may offend with this game.
+				</p>
+			)}
+			<p>...</p>
+		</div>
+	);
 }
 
 export default LevelsIndex;
